ebb9400de9b2882d15fc2a62c1a2b4c86d364750
Nick Marsceau <nickmarsceau@gmail.com>
2022-09-25 16:40:19 -0400

    Fix prop name

--- src/Elements/Input.js
+++ src/Elements/Input.js
@@ -2,7 +2,7 @@ import { randomString } from 'Utilities'
 
 export function Input({ type = 'text', id, placeholder, value, onChange, onEnter }) {
     return (
-        <input type={type} id={id} name={id} placeholder={placeholder} value={value} autocomplete={randomString()} onChange={onChange} onKeyUp={e => {
+        <input type={type} id={id} name={id} placeholder={placeholder} value={value} autoComplete={randomString()} onChange={onChange} onKeyUp={e => {
             if (e.key === 'Enter' && onEnter instanceof Function) {onEnter(e)}
         }} />
     )
